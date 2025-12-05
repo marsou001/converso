7be9bc955352fe70cb958fb49de38f41687dedd0
marsou001 <61951643+marsou001@users.noreply.github.com>
2025-12-05 21:39:45 +0100

    create companions squeleton in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard"
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+    </main>
   )
 }
 
---
+++ components/CompanionCard.tsx
@@ -0,0 +1,5 @@
+export default function CompanionCard() {
+  return (
+    <div>CompanionCard</div>
+  )
+}
