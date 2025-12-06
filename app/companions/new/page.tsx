5ce2103c7671d5c73d6ba9fd26d7bb2d96bdbfbc
marsou001 <61951643+marsou001@users.noreply.github.com>
2025-12-06 21:38:34 +0100

    set up companion adding form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,14 @@
+import CompanionForm from "@/components/CompanionForm";
+
 function Page() {
   return (
-    <div>Companions library New</div>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <div className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+
+        <CompanionForm />
+      </div>
+    </main>
   )
 }
 
---
+++ components/CompanionForm.tsx
@@ -0,0 +1,5 @@
+export default function CompanionForm() {
+  return (
+    <div>CompanionForm</div>
+  )
+}
