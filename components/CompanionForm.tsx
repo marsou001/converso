d874c6d5238b43097f8e5671114c8e413cd18fb5
marsou001 <61951643+marsou001@users.noreply.github.com>
2025-12-06 22:10:22 +0100

    add companion form schema

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,4 +1,33 @@
+"use client"
+
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const formSchema = z.object({
+  name: z.string().min(1, "Name is required."),
+  subject: z.enum(Object.values(Subject)),
+  topic: z.string().min(1, "Topic is required."),
+  voice: z.string().min(1, "Voice is required."),
+  style: z.string().min(1, "Style is required."),
+})
+
 export default function CompanionForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      subject: Subject.MATHS,
+      topic: "",
+      voice: "",
+      style: "",
+    },
+  })
+ 
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data)
+  }
+
   return (
     <div>CompanionForm</div>
   )
