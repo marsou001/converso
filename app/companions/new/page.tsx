aa6eec0a4ea904c53dee6ff32736a515226bb89a
marsou001 <61951643+marsou001@users.noreply.github.com>
2025-12-25 22:29:52 +0100

    improve component create form page layout

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -2,7 +2,7 @@ import CompanionForm from "@/components/CompanionForm";
 
 function Page() {
   return (
-    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+    <main className="min-lg:w-1/3 min-md:w-2/3 max-w-lg items-center justify-center">
       <div className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
 
